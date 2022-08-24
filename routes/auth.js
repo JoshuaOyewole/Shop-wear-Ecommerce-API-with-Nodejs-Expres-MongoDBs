4dfc45d07e588a5045bdb7cdd4588657e807db19
joshua Oyewole <joshpp2013@gmail.com>
2022-08-24 14:24:52 +0100

    added redirect to login auth

--- routes/auth.js
+++ routes/auth.js
@@ -48,7 +48,7 @@ router.post("/login", async (req, res) => {
 
     const { password, ...others } = user._doc;
 
-    res.status(200).json({...others, accessToken});
+    /* res.status(200).json({...others, accessToken}); */
     return res.redirect('/');
   } catch (err) {
     return res.status(500).json(err);
